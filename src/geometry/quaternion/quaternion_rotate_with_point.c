7197735feda5945c8686695ff9281caac26761f9
Thibault <farges.thibault@gmail.com>
2021-03-26 23:14:31 +0100

    Add Quat rotation to a given vector

---
+++ src/geometry/quaternion/quaternion_rotate_with_point.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#include "quaternion_exp.h"
+
+/**
+ * Applies quaternion rotation to a given vector.
+ */
+VOID_t
+quaternion_rotate_with_point(const Quaternion_t* q1, const Vect3D_t* point, Vect3D_t* v_output)
+{
+    return;
+}
